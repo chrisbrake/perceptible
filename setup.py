b8891413f2c811d8c50c68cd6ee9272890e2befd
chrisbrake <chris.brake@gmail.com>
2018-05-26 11:36:56 -0600

    Added long description

--- setup.py
+++ setup.py
@@ -1,13 +1,21 @@
 from distutils.core import setup
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
 setup(
-  name='perceptible',
-  packages=['perceptible'],
-  version='0.1',
-  description='Observable versions of python data structures',
-  author='Chris Brake',
-  author_email='chris.brake@gmail.com',
-  url='https://github.com/chrisbrake/perceptible',
-  download_url='https://github.com/chrisbrake/perceptible/archive/0.1.tar.gz',
-  keywords=['perceptible', 'observable', 'observer'],
-  classifiers=[],
+    name='perceptible',
+    packages=['perceptible'],
+    version='0.1',
+    description='Observable versions of python data structures',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author='Chris Brake',
+    author_email='chris.brake@gmail.com',
+    url='https://github.com/chrisbrake/perceptible',
+    download_url='https://github.com/chrisbrake/perceptible/archive/0.1.tar.gz',
+    keywords=['perceptible', 'observable', 'observer'],
+    classifiers=[],
 )
