c04ab119784310b9d3f6dbe574841d41d463a8fe
chrisbrake <nAp3ObtBG5yj>
2018-05-26 10:27:33 -0600

    Populated setup

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name='perceptible',
+  packages=['perceptible'],
+  version='0.1',
+  description='Observable versions of python data structures',
+  author='Chris Brake',
+  author_email='chris.brake@gmail.com',
+  url='https://github.com/chrisbrake/perceptible',
+  download_url='https://github.com/chrisbrake/perceptible/archive/0.1.tar.gz',
+  keywords=['perceptible', 'observable', 'observer'],
+  classifiers=[],
+)
