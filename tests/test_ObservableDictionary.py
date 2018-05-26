b6b887b90b1eb3996fb6cdcbd717038ff577a90a
chrisbrake <nAp3ObtBG5yj>
2018-05-26 10:58:43 -0600

    Fixed import

--- tests/test_ObservableDictionary.py
+++ tests/test_ObservableDictionary.py
@@ -1,6 +1,6 @@
 from mock import Mock
 from unittest import TestCase
-from observableDictionary.observableDictionary import ObservableDictionary
+from perceptible.observableDictionary import ObservableDictionary
 
 
 class TestObservableDictionary(TestCase):
