9d386e8c9bcbee8068355e0bf5bd9f33ffd19675
chrisbrake <chris.brake@gmail.com>
2018-05-28 21:47:22 -0600

    Marked noqa on import

--- docs/conf.py
+++ docs/conf.py
@@ -15,7 +15,7 @@
 import os
 import sys
 sys.path.insert(0, os.path.abspath('../../perceptible'))
-import perceptible
+import perceptible  # noqa: E402
 
 # -- Project information -----------------------------------------------------
 
